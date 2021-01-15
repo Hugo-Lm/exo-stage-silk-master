53c7e6d2c035d08f465a854a126f581a048cd83b
Hugo Le Morvan <lm.hugo@hotmail.fr>
2021-01-15 15:05:34 +0100

    exo 1.3 finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def style_color(name)
+    if name.title[0].downcase == "s"
+      return "text-warning "
+    elsif name.title.length % 2 == 0
+      return "text-danger"
+    elsif name.title.length % 2 != 0
+      return "text-primary"
+    end
+  end
 end
